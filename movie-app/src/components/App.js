146190671f064e87269e29ac9380b55848bf32fc
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-25 00:46:57 +0300

    Made the Search movie form

--- movie-app/src/components/App.js
+++ movie-app/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import Nav from './Nav'
+import Nav from './Nav';
+import SearchArea from './SearchArea';
 
 function App() {
   return (
     <div className="App">
         <Nav/>
+        <SearchArea/>
     </div>
   );
 }
