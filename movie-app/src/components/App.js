64099dc66e906b287cadef26c3acc3163a37b73d
Boyan <92653208+BoyanK95@users.noreply.github.com>
2022-10-25 02:21:36 +0300

    Fetching data from the Api

--- movie-app/src/components/App.js
+++ movie-app/src/components/App.js
@@ -2,13 +2,40 @@ import React, { Component } from 'react';
 import Nav from './Nav';
 import SearchArea from './SearchArea';
 
-function App() {
-  return (
-    <div className="App">
-        <Nav/>
-        <SearchArea/>
-    </div>
-  );
+class App extends Component{
+  constructor(){
+    super()
+    this.state = {
+      movies: [],
+      searchTerm: ''
+    }
+    this.apiKey = '089b617253e47ebe55e8aeef2a65814f'
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    fetch(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${this.state.searchTerm}`)
+    .then(data => data.json())
+    .then(data => {
+      console.log(data);
+      this.setState({movies: [...data.results]})
+    })
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchTerm: e.target.value })
+  }
+
+  render() {
+    return (
+      <div className="App">
+          <Nav/>
+          <SearchArea handleSubmit={this.handleSubmit} handleChange={this.handleChange}/>
+      </div>
+    );
+  }
 }
+
 
 export default App;
